0c8905cce94e7c6744b68e897ec0beb3b4786936
Colin Seymour <colin@symr.io>
2021-12-05 16:39:26 +0000

    Add more tweaks

--- chezmoi/.chezmoitemplates/firefox-user-overrides.js
+++ chezmoi/.chezmoitemplates/firefox-user-overrides.js
@@ -62,8 +62,13 @@ user_pref("accessibility.typeaheadfind.flashBar", 0); // I don't want the toolba
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // userchrome.css usercontent.css activate
 user_pref("svg.context-properties.content.enabled", true); // Fill SVG Color
 user_pref("layout.css.backdrop-filter.enabled", true); // CSS Blur Filter - 88 Above
+user_pref("layout.css.color-mix.enabled", true); // CSS Color Mix - 88 Above
+user_pref("browser.tabs.drawInTitlebar", true); // Draw in Titlebar
 user_pref("browser.compactmode.show", true); // Restore Compact Mode - 89 Above
 user_pref("browser.urlbar.suggest.calculator", false); // Integrated calculator at urlbar
 user_pref("security.default_personal_cert", "Select Automatically");
+user_pref("browser.proton.enabled", true); // Proton Enabled #127
+user_pref("browser.proton.places-tooltip.enabled", true); // Proton Tooltip
+user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false); // about:home Search Bar - 89 Above
 
 /*** Don't forget to remove and reset deprecated refs in the 9999 section of the default file ***/
