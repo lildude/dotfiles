e42648565cc73c8bca68daac9ba8f7e1afdc3967
Colin Seymour <colin@symr.io>
2025-12-05 13:33:09 +0000

    Update

--- chezmoi/dot_config/mise/activate.zsh
+++ chezmoi/dot_config/mise/activate.zsh
@@ -1,12 +1,14 @@
 export MISE_SHELL=zsh
-export __MISE_ORIG_PATH="$PATH"
+if [ -z "${__MISE_ORIG_PATH:-}" ]; then
+  export __MISE_ORIG_PATH="$PATH"
+fi
 export __MISE_ZSH_PRECMD_RUN=0
 
 mise() {
   local command
   command="${1:-}"
   if [ "$#" = 0 ]; then
-    command /opt/homebrew/bin/mise
+    command ${HOME}/.local/bin/mise
     return
   fi
   shift
@@ -15,22 +17,22 @@ mise() {
   deactivate|shell|sh)
     # if argv doesn't contains -h,--help
     if [[ ! " $@ " =~ " --help " ]] && [[ ! " $@ " =~ " -h " ]]; then
-      eval "$(command /opt/homebrew/bin/mise "$command" "$@")"
+      eval "$(command ${HOME}/.local/bin/mise "$command" "$@")"
       return $?
     fi
     ;;
   esac
-  command /opt/homebrew/bin/mise "$command" "$@"
+  command ${HOME}/.local/bin/mise "$command" "$@"
 }
 
 _mise_hook() {
-  eval "$(/opt/homebrew/bin/mise hook-env -s zsh)";
+  eval "$(${HOME}/.local/bin/mise hook-env -s zsh)";
 }
 _mise_hook_precmd() {
-  eval "$(/opt/homebrew/bin/mise hook-env -s zsh --reason precmd)";
+  eval "$(${HOME}/.local/bin/mise hook-env -s zsh --reason precmd)";
 }
 _mise_hook_chpwd() {
-  eval "$(/opt/homebrew/bin/mise hook-env -s zsh --reason chpwd)";
+  eval "$(${HOME}/.local/bin/mise hook-env -s zsh --reason chpwd)";
 }
 typeset -ag precmd_functions;
 if [[ -z "${precmd_functions[(r)_mise_hook_precmd]+1}" ]]; then
@@ -47,7 +49,7 @@ if [ -z "${_mise_cmd_not_found:-}" ]; then
     [ -n "$(declare -f command_not_found_handler)" ] && eval "${$(declare -f command_not_found_handler)/command_not_found_handler/_command_not_found_handler}"
 
     function command_not_found_handler() {
-        if [[ "$1" != "mise" && "$1" != "mise-"* ]] && /opt/homebrew/bin/mise hook-not-found -s zsh -- "$1"; then
+        if [[ "$1" != "mise" && "$1" != "mise-"* ]] && ${HOME}/.local/bin/mise hook-not-found -s zsh -- "$1"; then
           _mise_hook
           "$@"
         elif [ -n "$(declare -f _command_not_found_handler)" ]; then
